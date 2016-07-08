37bce1ba9ca8342274ba08a17379e17c5548462a
fdutra22 <f-dutra@live.com>
2016-07-08 17:58:09 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,28 @@
+
+<?php 
+    
+	$conn =  NEW PDO("mysql:host=localhost;dbname=osticket", "root", "root123");
+	
+	// Transformando arquivo XML em Objeto
+	$xml = simplexml_load_file('vendas.xml');
+ 
+	// Exibe as informações do XML
+	echo 'Título: ' . $xml->titulo . '<br>';
+	echo 'Data de Atualização: ' . $xml->data_atualizacao . '<br>';
+ 
+	// Percorre todos os registros de vendas
+foreach($xml->venda as $registro):
+	echo 'Código da Venda: ' . $registro->cod_venda . '<br>';
+	echo 'Cliente: ' . $registro->cliente . '<br>';
+	echo 'Email: ' . $registro->email . '<br>';
+        
+		
+        // Percorre todos os itens da venda
+	foreach($registro->itens->item as $item):
+		echo 'Código do Produto: ' . $item->cod_produto . '<br>';
+		echo 'Quantidade: ' . $item->qtde . '<br>';
+		echo 'Descrição do Produto: ' . $item->descricao . '<br>';
+	endforeach;
+ 
+	echo '<hr>';
+endforeach;
